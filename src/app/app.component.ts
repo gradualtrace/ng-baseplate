6229e40211321572d202ab4569fe76aeb6171332
Daemon Trace <gradualtrace@gmail.com>
2021-12-21 00:30:03 +0000

    Disable minimal app settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
   template: `
     <!--The content below is only a placeholder and can be replaced.-->
     <div style="text-align:center" class="content">
