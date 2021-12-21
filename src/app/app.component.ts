80667b76cbb65607aa00911590c3509d61a3187d
Daemon Trace <gradualtrace@gmail.com>
2021-12-21 01:55:10 +0000

    Specify scss as default CSS extension

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
   template: `
     <!--The content below is only a placeholder and can be replaced.-->
     <div style="text-align:center" class="content">
